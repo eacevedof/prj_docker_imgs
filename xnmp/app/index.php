e451701e720ce05bb142e7b9fd7a24419f37c190
Eduardo A.F <eacevedof@yahoo.es>
2019-12-27 19:42:12 +0100

    php no conecta con bd por driver

--- xnmp/app/index.php
+++ xnmp/app/index.php
@@ -1,4 +1,33 @@
 <?php
 echo "hello world in XNMP :)) !!";
 echo "<hr/>";
+
+function test_mysql()
+{
+    $host = "127.0.0.1";
+    $db   = "mysql";
+    $user = "root";
+    $pass = "1234";
+    $charset = "utf8mb4";
+    
+    $options = [
+        \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+        \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+        \PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    try {
+         $pdo = new \PDO($dsn, $user, $pass, $options);
+         $sql = "SELECT * FROM help_topic WHERE 1";
+         
+         $stmt = $pdo->query($sql);
+         while ($row = $stmt->fetch()) {
+             echo $row['name']."<br />\n";
+         }
+    } catch (\PDOException $e) {
+         throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    }    
+}//test_mysql()
+
+test_mysql();
 phpinfo();
