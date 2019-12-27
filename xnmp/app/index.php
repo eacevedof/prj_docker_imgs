30811380d2bf5ce339ef8ff45566d32caf7bbf74
Eduardo A.F <eacevedof@yahoo.es>
2019-12-27 20:28:36 +0100

    ya no da error de driver

--- xnmp/app/index.php
+++ xnmp/app/index.php
@@ -1,33 +1,38 @@
 <?php
-echo "hello world in XNMP :)) !!";
-echo "<hr/>";
-
 function test_mysql()
 {
-    $host = "127.0.0.1";
+    //$host = "127.0.0.1";
+    $host = "cmari1"; //nombre del contenedor (docker ps -> NAMES)
     $db   = "mysql";
     $user = "root";
     $pass = "1234";
-    $charset = "utf8mb4";
-    //var_export(\PDO::ATTR_FETCH_ASSOC);die;
+    $charset = "utf8";
+
     $options = [
         \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
         \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
         \PDO::ATTR_EMULATE_PREPARES   => false,
     ];
+
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-    try {
-         $pdo = new \PDO($dsn, $user, $pass, $options);
-         $sql = "SELECT * FROM help_topic WHERE 1";
-         
-         $stmt = $pdo->query($sql);
-         while ($row = $stmt->fetch()) {
-             echo $row['name']."<br />\n";
-         }
-    } catch (\PDOException $e) {
-         throw new \PDOException($e->getMessage(), (int)$e->getCode());
-    }    
+    try 
+    {
+        $pdo = new \PDO($dsn, $user, $pass, $options);
+        $sql = "SELECT * FROM help_topic WHERE 1";
+        echo "<pre>";
+        echo "<b>$sql</b>\n";
+        $stmt = $pdo->query($sql);
+        while ($row = $stmt->fetch()) 
+        {
+            print_r($row);
+        }
+    } 
+    catch (\PDOException $e) 
+    {
+        throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    }
 }//test_mysql()
 
+echo "<pre><b>hello world in XNMP :) !!</b></pre><br/>";
 test_mysql();
 phpinfo();
