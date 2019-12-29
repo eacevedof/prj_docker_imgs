9626b5ebb8e92a292cb0435e4fcdb234ae3b74ff
Eduardo A.F <eacevedof@yahoo.es>
2019-12-29 12:00:18 +0100

    symf01 paso logia al entrypoint

--- symf01/php7_fpm/entrypoint.sh
+++ symf01/php7_fpm/entrypoint.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+echo "START entrypoint.sh"
+
+cd /var/www/html
+
+if [ ! -d symsite ]; then
+  composer create-project symfony/website-skeleton symsite
+fi
+
+# arranca el servicio
 php -S 0.0.0.0:8000 -t /var/www/html/symsite/public
+
+echo "END entrypoint.sh"
