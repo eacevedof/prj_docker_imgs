ca68fd58bd740948a515db5c2991bdf162bb6ae6
Eduardo A.F <eacevedof@yahoo.es>
2019-12-29 12:23:24 +0100

    symf01 mejoro entrypoint

--- symf01/php7_fpm/entrypoint.sh
+++ symf01/php7_fpm/entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-echo "START entrypoint.sh"
+echo "entrypoint.sh: START"
 
 if [ ! -d /var/www/html/symsite ]; then
+  echo "entrypoint.sh: Instalando symsite"
   (cd /var/www/html/ && composer create-project symfony/website-skeleton symsite)
+  echo "entrypoint.sh: Fin instalación symsite"
+else
+  echo "entrypoint.sh: Ya existe symsite"
 fi
 
 # arranca el servicio
 php -S 0.0.0.0:8000 -t /var/www/html/symsite/public
 
-echo "END entrypoint.sh"
+echo "entrypoint.sh: END"
