46801065cc4733b4e1c11d0555a283491b99fd0b
Eduardo A.F <eacevedof@yahoo.es>
2020-01-04 11:38:38 +0100

    ya arranca el servicio

--- symf02/www/html/entrypoint.sh
+++ symf02/www/html/entrypoint.sh
@@ -9,7 +9,9 @@ else
   echo "entrypoint.sh: Ya existe symsite"
 fi
 
-# arranca el servicio
-# php -S 0.0.0.0:8000 -t /var/www/html/symsite/public
+# como estoy rescribiendo el entrypoint por defecto de la imagen tengo que lanzar 
+# los mismos servicios. Este habilita la escucha de fastcgi por el puerto 9000
+# que es el que usa nginx para servir el php
+docker-php-entrypoint php-fpm
 
 echo "entrypoint.sh: END"
