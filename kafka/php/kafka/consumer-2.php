5a9fecaa5d29f1c57d726293246f35f4d477f28e
ioedumac <eacevedof@yahoo.es>
2021-06-12 22:09:22 +0200

    con consumer-2 tampoco recibe datos pero no da error

--- kafka/php/kafka/consumer-2.php
+++ kafka/php/kafka/consumer-2.php
@@ -5,8 +5,9 @@ echo "=============\n";
 
 $conf = new RdKafka\Conf();
 $conf->set("bootstrap.servers", $KAFKA_SOCKET);
+
 $consumer = new RdKafka\Consumer($conf);
-//$consumer->setLogLevel(LOG_DEBUG);
+$consumer->setLogLevel(LOG_DEBUG);
 $consumer->addBrokers($KAFKA_SOCKET);
 
 $topic = $consumer->newTopic(KAFKA_TOPIC);
@@ -19,8 +20,8 @@ while (true) {
 
 /*
 %5|1623527935.432|CONFWARN|rdkafka#consumer-1| [thrd:app]: No `bootstrap.servers` configured: client will not be able to connect to Kafka cluster
-consumer startedPHP Warning:  Attempt to read property "payload" on null in /Users/ioedu/projects/prj_docker_imgs/kafka/php/kafka/consumer-2.php on line 17
 
+//esto $msg->payload
 Warning: Attempt to read property "payload" on null in /Users/ioedu/projects/prj_docker_imgs/kafka/php/kafka/consumer-2.php on line 17
 PHP Warning:  Attempt to read property "payload" on null in /Users/ioedu/projects/prj_docker_imgs/kafka/php/kafka/consumer-2.php on line 17
 
