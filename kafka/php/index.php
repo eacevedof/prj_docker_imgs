54c131d4c0d6e00933723e2accd65f232080ca83
ioedumac <eacevedof@yahoo.es>
2021-06-16 19:40:20 +0200

    get_filename

--- kafka/php/index.php
+++ kafka/php/index.php
@@ -3,19 +3,8 @@ $action = $_GET["action"] ?? "";
 if(!$action) {
     $action = $argv[1];
 }
-if ($action==="p")
-{
-    include_once("kafka/producer.php");
-}
-elseif($action==="c")
-{
-    include_once("kafka/consumer.php");
-}
-else
-{
-    //include_once("kafka/consumer.php");
-    //include_once("kafka/consumer-2.php");
-    //include_once("kafka/consumer-3.php");
-}
+$filename = get_filename($action);
+$path = "kafka/$filename";
+include_once($path);
 
 echo sprintf("%s:%s -> topic: %s",KAFKA_SERVER,KAFKA_PORT,KAFKA_TOPIC);
