f568763a9e764d188ec2af132fd8e6ec46df26bd
ioedumac <eacevedof@yahoo.es>
2021-06-16 23:14:56 +0200

    descriebe.sh

--- kafka/docker/kafka/bash/describe.sh
+++ kafka/docker/kafka/bash/describe.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-describe_topic_config "test" | awk 'BEGIN{IFS="=";IRS=" "} /^[ ]*retention.ms/{print $1}'
+# es una func sacada de aqui: https://www.baeldung.com/kafka-message-retention
+#describe_topic_config "test" | awk 'BEGIN{IFS="=";IRS=" "} /^[ ]*retention.ms/{print $1}'
+kafka-configs.sh --describe --all \
+      --bootstrap-server=0.0.0.0:9092 \
+      --topic ${topic_name}
