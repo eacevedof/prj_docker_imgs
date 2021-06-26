5eac3aa4da55cd3c704929c16757f735ec568a67
ioedumac <eacevedof@yahoo.es>
2021-06-26 15:18:03 +0200

    redis comentario .sh

--- redis/docker/redis/bash/show_size.sh
+++ redis/docker/redis/bash/show_size.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+# muestra el tamaño ocupado por cada key
 get_size() {
   k=$1
   r=$(redis-cli MEMORY USAGE "$k")
   echo $r
 }
 
-redis_cmd='redis-cli'
+redis_cmd="redis-cli"
 
 for k in `$redis_cmd keys "*"`;
 do
