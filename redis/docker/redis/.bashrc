f6d10ac143d1e3659ce34fc8923cba5348519cbf
ioedumac <eacevedof@yahoo.es>
2021-06-26 11:40:02 +0200

    refactor mejorado incluyo get-size

--- redis/docker/redis/.bashrc
+++ redis/docker/redis/.bashrc
@@ -1,2 +1,6 @@
 export PATHBASH="$HOME/bash"
 export PATH=$PATH:$PATHBASH
+
+alias ll='l -l'
+alias keys=redis-cli
+alias get-size=show_size.sh
---
+++ redis/docker/redis/bash/show_size.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+get_size() {
+  k=$1
+  r=$(redis-cli MEMORY USAGE "$k")
+  echo $r
+}
+
+redis_cmd='redis-cli'
+
+for k in `$redis_cmd keys "*"`;
+do
+  echo $k
+  get_size $k
+done
