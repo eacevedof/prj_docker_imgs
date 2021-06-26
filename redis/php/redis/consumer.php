e9e4800a2eb9e8927e4b3f8b63259c0e4725de49
ioedumac <eacevedof@yahoo.es>
2021-06-26 11:10:46 +0200

    todo ok, lee y escribe

--- redis/php/redis/consumer.php
+++ redis/php/redis/consumer.php
@@ -8,4 +8,8 @@ $redis = new Redis();
 $redis->connect(getenv("REDIS_SERVER"), getenv("REDIS_PORT"));
 //$array = $redis->lRange("some-key",0,-1);
 $array = $redis->keys("*");
-print_r($array);
+foreach ($array as $key)
+{
+    $value = $redis->get($key);
+    echo "\n$key: `$value`\n";
+}
--- redis/php/redis/producer.php
+++ redis/php/redis/producer.php
@@ -6,11 +6,14 @@ PRODUCER
 ";
 $redis = new Redis();
 $redis->connect(getenv("REDIS_SERVER"), getenv("REDIS_PORT"));
-for($i=0; $i<10; $i++)
+for($i=0; $i<100; $i++)
 {
     $uuid = uniqid();
-    $key = "id-$i-$uuid";
-    $value = "some rare value in string ".date("Y-m-d H:i:s");
+    $key = "id-$i";
+    $ttl = $i + 50;
+    $date = new \DateTime("+$ttl seconds");
+    $enddate = $date->format("H:i:s");
+    $value = "some rare value in string ".date("Y-m-d H:i:s")." - $enddate";
     $redis->set($key, $value);
-    $redis->expire($key, $i+30);
+    $redis->expire($key, $ttl);
 }
