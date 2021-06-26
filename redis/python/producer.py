1714b26971fa0d72da5b6b2bb5a3db77961c228c
ioedumac <eacevedof@yahoo.es>
2021-06-26 11:47:19 +0200

    dump.rdb ignore redis

--- redis/python/producer.py
+++ redis/python/producer.py
@@ -1,13 +1,4 @@
-from time import sleep
-from json import dumps
-from redis import RedisProducer
+import os
+import redis
 
-producer = RedisProducer(bootstrap_servers=["localhost:9092"],
-                         value_serializer=lambda x: 
-                         dumps(x).encode("utf-8"))
-
-for e in range(50):
-    #data = {"number" : e}
-    data = e
-    producer.send("test", value=data)
-    sleep(5)
+SERVER = os.getenv()
