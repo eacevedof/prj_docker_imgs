5bee57c020a1235f0c3cf13180a486e7417b02e2
ioedumac <eacevedof@yahoo.es>
2022-12-06 16:20:18 +0100

    php connect

--- xnp-mssql/app/index.php
+++ xnp-mssql/app/index.php
@@ -1,14 +1,46 @@
 <?php
-//vscode con xdebug
-$myarray = [
-  "a" => "some string",
-  "b" => ["other", "array"]
-];
+function test_mssql()
+{
+    $host = "cont-mssql";
+    $db   = "db_eduardoaf";
+    $user = "sa";
+    $pass = "sa";
+    $charset = "utf8";
 
-$mydomain = "eduardoaf.com";
+    $options = [
+        \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+        \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+        \PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
 
-$obj = new stdClass();
-$obj->data = $myarray;
-$obj->domain = $mydomain;
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    try 
+    {
+        $pdo = new \PDO($dsn, $user, $pass, $options);
+        $sql = "
+          SELECT name AS object_name
+          ,SCHEMA_NAME(schema_id) AS schema_name
+          ,type_desc
+          ,create_date
+          ,modify_date
+          FROM sys.objects
+          WHERE modify_date > GETDATE() - <n_days>
+          ORDER BY modify_date;
+        ";
+        echo "<pre>";
+        echo "<b>$sql</b>\n";
+        $stmt = $pdo->query($sql);
+        while ($row = $stmt->fetch()) 
+        {
+            print_r($row);
+        }
+        echo "</pre>";
+    } 
+    catch (\PDOException $e) 
+    {
+        throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    }
+}//test_mssql()
 
-echo xdebug_info();
+test_mssql();
+phpinfo();
